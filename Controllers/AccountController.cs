a42c4347757ea216ddc3d2bcd5795c28c1e17492
Keren <131375714+KerenRivera@users.noreply.github.com>
2026-04-03 02:32:39 -0400

    add simple login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Book_manager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username)
+        {
+            if (!string.IsNullOrEmpty(username))
+            {
+                HttpContext.Session.SetString("Username", username);
+                return RedirectToAction("Index", "Books");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
